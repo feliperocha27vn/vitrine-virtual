5e564363a76eced0a5c9cd8f32dbdad141271df8
Felipe Rocha <feliperocha.dev27@gmail.com>
2025-04-26 13:36:04 -0300

    feat: rota para persistencia de dados das marcas, importacao de dependencias no server, para controle de documentacao

---
+++ backend/src/routes/brand-insert-route.ts
@@ -0,0 +1,31 @@
+import z from 'zod'
+import { prisma } from '../prisma/prisma'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+
+export const brandInsertRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/marca',
+    {
+      schema: {
+        summary: 'Rota responsável pela criação de marcas',
+        body: z.object({
+          id: z.string().uuid(),
+          nome: z
+            .string()
+            .min(1, { message: 'O campo do nome da marca é obrigatório!' }),
+        }),
+      },
+    },
+    async (request, response) => {
+      const { nome } = request.body
+
+      await prisma.marca.create({
+        data: {
+          nome,
+        },
+      })
+
+      response.status(201).send({ message: 'Marca criada com sucesso!' })
+    }
+  )
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,34 @@
+import fastifySwagger from '@fastify/swagger'
 import { app } from './app'
+import { productInsertRoute } from './routes/product-insert-route'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { brandInsertRoute } from './routes/brand-insert-route'
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'API documentation for the Virtrine Perola backend',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(brandInsertRoute)
+app.register(productInsertRoute)
 
 app
   .listen({
